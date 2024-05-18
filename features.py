919ea0070beb9d91877169dc079ab2a06f687047
bryanjaysonfuchs <bryan.fuchs@hispeed.ch>
2024-05-18 13:05:00 +0200

    randomforest.py model improvements

--- features.py
+++ features.py
@@ -4,48 +4,60 @@ import os
 
 script_path = os.path.abspath(__file__)
 directory = os.path.dirname(script_path)
-config = f"{directory}\\config\\"
-storage = f"{directory}\\data\\"
+config = os.path.join(directory, 'config')
+storage = os.path.join(directory, 'data')
 
-data_all = pd.read_excel(f"{storage}commodity_data.xlsx")
+data_all = pd.read_excel(os.path.join(storage, 'raw', 'commodity_data.xlsx'))
 data_all = data_all.iloc[::-1]
 
-data_all["Dates"] = pd.to_datetime(data_all["Dates"])
-data_all["Dates"] = data_all["Dates"].dt.strftime('%Y-%m-%d')
-data_all.set_index("Dates", inplace=True)
+data_all['Dates'] = pd.to_datetime(data_all['Dates'])
+data_all['Dates'] = data_all['Dates'].dt.strftime('%Y-%m-%d')
+data_all.set_index('Dates', inplace=True)
 
-commodities = ["LC1", "CO1", "CL1", "CT1", "NG1", "HG1", "W1", "GC1", "S1"]
+commodities = ['LC1', 'CO1', 'CT1', 'NG1', 'HG1', 'W1', 'GC1', 'S1']
 column_ranges = {
-    "LC1": list(range(0, 6)),
-    "CO1": list(range(6, 12)),
-    "CL1": list(range(12, 18)),
-    "CT1": list(range(18, 24)),
-    "NG1": list(range(24, 30)),
-    "HG1": list(range(30, 36)),
-    "W1": list(range(36, 42)),
-    "GC1": list(range(42, 48)),
-    "S1": list(range(48, 54))}
+    'LC1': list(range(0, 6)),
+    'CO1': list(range(6, 12)),
+    'CL1': list(range(12, 18)),
+    'CT1': list(range(18, 24)),
+    'NG1': list(range(24, 30)),
+    'HG1': list(range(30, 36)),
+    'W1': list(range(36, 42)),
+    'GC1': list(range(42, 48)),
+    'S1': list(range(48, 54))}
 
 for commodity in commodities:
     cols = data_all.columns[column_ranges[commodity]].tolist()
     data = data_all.loc[:, cols]
 
-    # Setting target (1 means price tomorrow is higher than price today)
-    data["Tomorrow"] = data[f"{commodity}_PX_LAST"].shift(-1)
-    data["Target"] = (data["Tomorrow"] > data[f"{commodity}_PX_LAST"]).astype(int)
+    # Setting target (1 means price tomorrow is higher than price today), calculating returns
+    data['Price_Tomorrow'] = data[f'{commodity}_PX_LAST'].shift(-1)
+    data['Direction_Tomorrow'] = (data['Price_Tomorrow'] > data[f'{commodity}_PX_LAST']).astype(int)
+    data['Direction'] = data['Direction_Tomorrow'].shift(1)
+    data['Log_Returns'] = np.log(data[f'{commodity}_PX_LAST'] / data[f'{commodity}_PX_LAST'].shift(1))
+    data['Ret_Tomorrow'] = data['Log_Returns'].shift(-1)
 
-    horizons = [5, 10, 15]
+    horizons = [7, 14, 21]
 
     for horizon in horizons:
         moving_average = data.rolling(horizon).mean()
-        col_name = f"{commodity}_MA_{horizon}"
+        col_name = f'{commodity}_MA_{horizon}'
 
-        data[col_name] = moving_average[f"{commodity}_PX_LAST"]
+        data[col_name] = moving_average[f'{commodity}_PX_LAST']
+        data[f'{commodity}_SD_{horizon}'] = data[f'{commodity}_PX_LAST'].rolling(horizon).std()
 
-    data[f"{commodity}_SD_5"] = data[f"{commodity}_PX_LAST"].rolling(5).std()
-    data[f"{commodity}_HL"] = data[f"{commodity}_PX_HIGH"] - data[f"{commodity}_PX_LOW"]
-    data[f"{commodity}_OC"] = data[f"{commodity}_PX_OPEN"] - data[f"{commodity}_PX_LAST"]
+    data[f'{commodity}_PX_Lag_1'] = data[f'{commodity}_PX_LAST'].shift(1)
+    data[f'{commodity}_PX_Lag_2'] = data[f'{commodity}_PX_LAST'].shift(2)
+    data[f'{commodity}_EMA'] = data[f'{commodity}_PX_LAST'].ewm(span=5).mean()
+    data[f'{commodity}_HL'] = data[f'{commodity}_PX_HIGH'] - data[f'{commodity}_PX_LOW']
+    data[f'{commodity}_OC'] = data[f'{commodity}_PX_OPEN'] - data[f'{commodity}_PX_LAST']
 
-    data.to_pickle(f"{storage}{commodity}.pkl")
+    data.dropna(inplace=True)
+
+    data.to_pickle(os.path.join(storage, 'raw', f'{commodity}.pkl'))
+    print(commodity)
+
+
+#### IMPLEMENT SCALING FOR NN
 
 print()
--- randomforest.py
+++ randomforest.py
@@ -1,55 +1,74 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 import os
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import precision_score
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingClassifier
 
 script_path = os.path.abspath(__file__)
 directory = os.path.dirname(script_path)
-config = f"{directory}\\config\\"
-storage = f"{directory}\\data\\"
+config = os.path.join(directory, 'config')
+storage = os.path.join(directory, 'data')
 
-commodities = ["LC1", "CO1", "CL1", "CT1", "NG1", "HG1", "W1", "GC1", "S1"]
-commodity = "NG1"
+commodities = ['LC1', 'CO1', 'CT1', 'NG1', 'HG1', 'W1', 'GC1', 'S1']
 
-data = pd.read_pickle(f"{storage}{commodity}.pkl")
+n_est = 500
 
-predictors = [f"{commodity}_MA_5", f"{commodity}_MA_10", f"{commodity}_MA_15",
-              f"{commodity}_SD_5", f"{commodity}_HL", f"{commodity}_OC"]
+model_class = GradientBoostingClassifier(n_estimators=n_est, min_samples_split=2, max_depth=10, random_state=2)
+model_reg = GradientBoostingRegressor(n_estimators=n_est, min_samples_split=2, max_depth=10, random_state=2)
 
-predictors = [f"{commodity}_PX_LAST", f"{commodity}_PX_LOW", f"{commodity}_PX_HIGH", f"{commodity}_PX_OPEN",
-              f"{commodity}_PX_VOLUME", f"{commodity}_OPEN_INT"]
 
-train = data.iloc[:-250]
-test = data.iloc[-250:]
+def predict_dir(train, test, predictors, model, confidence=0.6):
+    model.fit(train[predictors], train['Direction_Tomorrow'])
+    predictions_dir = model.predict_proba(test[predictors])[:, 1]
+    predictions_dir[predictions_dir >= confidence] = 1
+    predictions_dir[predictions_dir < confidence] = 0
+    predictions_dir = pd.Series(predictions_dir, index=test.index, name='Predictions_Dir')
+    combined_dir = pd.concat([test['Direction'], predictions_dir], axis=1)
+    return combined_dir
 
-model = RandomForestClassifier(n_estimators=1000, min_samples_split=100, random_state=13)
 
-def predict(train, test, predictors, model):
-    model.fit(train[predictors], train["Target"])
-    preds = model.predict_proba(test[predictors])[:, 1]
-    preds[preds >= 0.6] = 1
-    preds[preds < 0.6] = 0
-    preds = pd.Series(preds, index=test.index, name="Predictions")
-    combined = pd.concat([test["Target"], preds], axis=1)
-    return combined
+def predict_price(train, test, predictors, model):
+    model.fit(train[predictors], train['Price_Tomorrow'])
+    predictions_price = model.predict(test[predictors])
+    predictions_price = pd.Series(predictions_price, index=test.index, name='Predictions_Price')
+    combined_price = pd.concat([test['Price_Tomorrow'], predictions_price], axis=1)
+    return combined_price
 
-def backtest(data, model, predictors, start=2500, step=250):
-    all_predictions = []
 
-    for i in range(start, data.shape[0], step):
-        train = data.iloc[0:i].copy()
-        test = data.iloc[i:(i+step)].copy()
-        predictions = predict(train, test, predictors, model)
-        all_predictions.append(predictions)
+def predict_ret(train, test, predictors, model):
+    model.fit(train[predictors], train['Ret_Tomorrow'])
+    predictions_return = model.predict(test[predictors])
+    predictions_return = pd.Series(predictions_return, index=test.index, name='Predictions_Ret')
+    combined_ret = pd.concat([test['Ret_Tomorrow'], predictions_return], axis=1)
+    return combined_ret
 
-    return pd.concat(all_predictions)
 
-predictions = backtest(data, model, predictors)
+def backtest(data, model_class, model_reg, predictors, t_set_size=0.8, confidence=0.55):
+    split_point = int(len(data) * t_set_size)
+    train, test = data[:split_point], data[split_point:]
 
-print(predictions["Predictions"].value_counts())
-print(precision_score(predictions["Target"], predictions["Predictions"]))
-print(predictions["Target"].value_counts() / predictions.shape[0])
+    predictions_dir = predict_dir(train, test, predictors, model_class, confidence=confidence)
+    predictions_price = predict_price(train, test, predictors, model_reg)
+    predictions_ret = predict_ret(train, test, predictors, model_reg)
+    predictions_all = pd.concat([predictions_price, predictions_ret, predictions_dir], axis=1)
 
-print()
+    rename = ['Price', 'Pred_Price', 'Log_Ret', 'Pred_Log_Ret', 'Dir', 'Pred_Dir']
+    predictions_all.columns = rename
+
+    predictions_all.to_pickle(os.path.join(storage, 'predictions', 'rf', f'{commodity}_rf_pred.pkl'))
+
+    return predictions_all
+
+
+for commodity in commodities:
+    print(commodity)
+    data = pd.read_pickle(os.path.join(storage, 'raw', f'{commodity}.pkl'))
+    data.dropna()
+
+    predictors = [f'{commodity}_MA_7', f'{commodity}_MA_14', f'{commodity}_MA_21', f'{commodity}_SD_7',
+                  f'{commodity}_SD_14', f'{commodity}_HL', f'{commodity}_OC', f'{commodity}_PX_VOLUME',
+                  f'{commodity}_PX_LAST', f'{commodity}_PX_HIGH', f'{commodity}_PX_LOW', f'{commodity}_EMA',
+                  f'{commodity}_PX_Lag_1', f'{commodity}_PX_Lag_2']
+
+    rf_predictions = backtest(data, model_class, model_reg, predictors, confidence=0.55)
